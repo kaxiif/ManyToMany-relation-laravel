7c40d5cc1b302c481f4c698905ad1b6aa1ae7334
kashif.hussain <kashiemails@gmail.com>
2022-03-22 10:37:33 +0500

    CRUD and attach-detach added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,57 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function () {
+    $user =User::find(1);
+    $user->roles()->save(new Role(['name'=>'Administrator']));
+    
+});
+
+
+Route::get('/read',function(){
+    $user = User::findOrFail(1);
+    foreach($user->roles as $role){
+        dd($role);
+    }
+});
+
+Route::get('/update',function(){
+    $user = User::findOrFail(1);
+
+    if($user->has('roles')){
+        foreach($user->roles as $role){
+            if($role->name== 'Administrator'){
+                $role->name="subscriber";
+                $role->save();
+            }
+        }
+    }
+});
+
+
+Route::get('/delete',function(){
+    $user = User::findOrFail(1);
+    foreach($user->roles as $role){
+        $role->whereId(1)->delete();
+    }
+});
+
+
+Route::get('/attach',function(){
+
+    $user = User::findOrFail(1);
+    $user->roles()->attach(3);
+
+
+});
+
+
+Route::get('/detach',function(){
+
+    $user = User::findOrFail(1);
+    $user->roles()->detach(3);
+
+
 });
